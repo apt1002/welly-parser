821a22a34259494245f3349df2e3711f103d2498
Alistair Turnbull <github@minworks.co.uk>
2024-09-27 20:43:45 +0100

    Rename to `welly-parser`. Flesh out `Cargo.toml` for publication.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{io};
 
 fn main() -> std::io::Result<()> {
     println!("Welly!");
-    wparser::echo(&mut io::stdin().lock(), &mut io::stdout())?;
+    welly_parser::echo(&mut io::stdin().lock(), &mut io::stdout())?;
     Ok(())
 }
 
