309e903b20afc045cf4270c8874e80219754c368
Alistair Turnbull <apt1002@sheep>
2025-12-15 03:19:55 +0000

    Implement validating `if`, `while`, `for` and `else`.

--- src/ast/stmt.rs
+++ src/ast/stmt.rs
@@ -5,18 +5,21 @@ use enums::{Separator, Op, ItemWord};
 use loc::{Location, Loc, Locate};
 use parser::{Doc, Item};
 
-pub const BAD_NAME: &'static str = "Expected a name";
-pub const BAD_TAG: &'static str = "Expected a TAG";
-pub const BAD_CALL: &'static str = "Expected ( ... )";
+pub const BAD_NAME: &'static str = "Expected `name`";
+pub const BAD_TAG: &'static str = "Expected `TAG`";
+pub const BAD_IN: &'static str = "Expected `pattern in sequence`";
+pub const BAD_CALL: &'static str = "Expected `( ... )`";
 pub const MISSING_LHS: &'static str = "Expected a pattern before this assignment operator";
 pub const MISSING_RHS: &'static str = "Expected an expression after this assignment operator";
-pub const MISSING_NAME: &'static str = "Expected a name after this keyword";
-pub const MISSING_CALL: &'static str = "Expected expression( ... )";
+pub const MISSING_NAME: &'static str = "Expected `name` after this keyword";
+pub const MISSING_CALL: &'static str = "Expected `expression( ... )`";
 pub const MISSING_EXPR: &'static str = "Expected an expression after this keyword";
-pub const MISSING_BLOCK: &'static str = "Expected { ... } after this statement";
-pub const EXTRA_BLOCK: &'static str = "Unexpected { ... }";
+pub const MISSING_BLOCK: &'static str = "Expected `{ ... }` after this statement";
+pub const EXTRA_BLOCK: &'static str = "Unexpected `{ ... }`";
 pub const MISSING_STMT: &'static str = "Expected a statement";
-pub const MISSING_SEMICOLON: &'static str = "This statement must be followed by a semicolon";
+pub const EXTRA_EXPR: &'static str = "Unexpected expression";
+pub const EXTRA_ELSE: &'static str = "`else` must follow an `if`, `while` or `for` statement";
+pub const MISSING_SEMICOLON: &'static str = "This statement must be followed by `;`";
 
 // ----------------------------------------------------------------------------
 
@@ -34,6 +37,15 @@ fn to_tag(expr: Expr) -> loc::Result<Loc<Tag>> {
     let ret = match expr {
         Expr::Tag(tag) => tag,
         expr => Err(Loc(BAD_TAG, expr.loc()))?,
+    };
+    Ok(ret)
+}
+
+/// Checks that`expr` is of the form `pattern in sequence`.
+fn remove_in(expr: Expr) -> loc::Result<(Pattern, Expr)> {
+    let ret = match expr {
+        Expr::Op(Some(pattern), Loc(Op::In, _), Some(sequence)) => (Pattern::from_expr(*pattern)?, *sequence),
+        expr => Err(Loc(BAD_IN, expr.loc()))?,
     };
     Ok(ret)
 }
@@ -57,6 +69,23 @@ fn remove_call(expr: Expr) -> loc::Result<(Option<Expr>, Expr)> {
     };
     if !matches!(&expr, Expr::Group(_) | Expr::Tuple(_)) { Err(Loc(BAD_CALL, expr.loc()))? }
     Ok((function, expr))
+}
+
+// ----------------------------------------------------------------------------
+
+/// Distinguishes a  [`Stmt::Control`].
+#[derive(Debug, Clone)]
+pub enum Control {
+    /// `if condition { ... } else { ... }` executes the body or the [`Else`].
+    If(Box<Expr>),
+
+    /// `while condition { ... } else { ... }` executes the body repeatedly
+    /// then the [`Else`] (unless the body jumps somewhere else).
+    While(Box<Expr>),
+
+    /// `for pattern in sequence { ... } else { ... }` executes the body
+    /// repeatedly then the [`Else`] (unless the body jumps somewhere else).
+    For(Box<Pattern>, Box<Expr>),
 }
 
 // ----------------------------------------------------------------------------
@@ -88,6 +117,10 @@ pub enum Stmt {
     /// `case TAG( ... ) { ... }` is a target for `match`.
     Case(Location, Loc<Tag>, Box<Pattern>, Loc<Block>),
 
+    /// Execute `body` and/or `else` depending on `control`.
+    /// The `else` [`Block`] is optional.
+    Control(Location, Control, Loc<Block>, Option<Loc<Block>>),
+
     /// `{ ... }` executes the statements in the `Block`.
     Block(Loc<Block>),
 }
@@ -115,6 +148,21 @@ impl Stmt {
     pub fn case(word: Location, tag: Loc<Tag>, parameter: Pattern, block: Loc<Block>) -> Self {
         Self::Case(word, tag, Box::new(parameter), block)
     }
+
+    /// Constructs a `Self::Control` containing a `Control::If`.
+    pub fn if_(word: Location, condition: Expr, body: Loc<Block>) -> Self {
+        Self::Control(word, Control::If(Box::new(condition)), body, None)
+    }
+
+    /// Constructs a `Self::Control` containing a `Control::While`.
+    pub fn while_(word: Location, condition: Expr, body: Loc<Block>) -> Self {
+        Self::Control(word, Control::While(Box::new(condition)), body, None)
+    }
+
+    /// Constructs a `Self::Control` containing a `Control::For`.
+    pub fn for_(word: Location, pattern: Pattern, sequence: Expr, body: Loc<Block>) -> Self {
+        Self::Control(word, Control::For(Box::new(pattern), Box::new(sequence)), body, None)
+    }
 }
 
 impl Locate for Stmt {
@@ -126,6 +174,7 @@ impl Locate for Stmt {
             Self::Return(word, _) => word.loc_start(),
             Self::Match(word, _) => word.loc_start(),
             Self::Case(word, _, _, _) => word.loc_start(),
+            Self::Control(word, _, _, _) => word.loc_start(),
             Self::Block(block) => block.loc_start(),
         }
     }
@@ -139,6 +188,8 @@ impl Locate for Stmt {
             Self::Return(word, None) => word.loc_end(),
             Self::Match(_, expr) => expr.loc_end(),
             Self::Case(_, _, _, block) => block.loc_end(),
+            Self::Control(_, _, _, Some(block)) => block.loc_end(),
+            Self::Control(_, _, block, None) => block.loc_end(),
             Self::Block(block) => block.loc_end(),
         }
     }
@@ -146,14 +197,31 @@ impl Locate for Stmt {
 
 impl Validate<Item> for Stmt {
     fn validate(tree: &Item) -> loc::Result<Self> {
+        let ret = match StmtOrElse::validate(tree)? {
+            StmtOrElse::Stmt(stmt) => stmt,
+            StmtOrElse::Else(word, _) => Err(Loc(EXTRA_ELSE, word))?,
+        };
+        Ok(ret)
+    }
+}
+
+// ----------------------------------------------------------------------------
+
+/// Represents a `Stmt` or `else { ... }`.
+#[derive(Debug, Clone)]
+pub enum StmtOrElse { Stmt(Stmt), Else(Location, Loc<Block>) }
+
+impl Validate<Item> for StmtOrElse {
+    fn validate(tree: &Item) -> loc::Result<Self> {
         let ret = match tree {
-            Item::Eval(formula) => Self::Expr(Expr::validate(formula)?),
+            Item::Separator(separator) => Err(Loc(MISSING_STMT, separator.loc()))?,
+            Item::Eval(formula) => Stmt::Expr(Expr::validate(formula)?),
             Item::Assign(lhs, op, rhs) => {
                 let Some(lhs) = lhs else { Err(Loc(MISSING_LHS, op.1))? };
                 let Some(rhs) = rhs else { Err(Loc(MISSING_RHS, op.1))? };
                 let lhs = Pattern::validate(lhs)?;
                 let rhs = Expr::validate(rhs)?;
-                Self::assign(lhs, *op, rhs)
+                Stmt::assign(lhs, *op, rhs)
             },
             Item::Verb(word, formula, bracket) => {
                 let expr = Option::<Expr>::validate(formula)?;
@@ -162,7 +230,7 @@ impl Validate<Item> for Stmt {
                     ItemWord::Module => {
                         let name = if let Some(expr) = expr { Some(to_name(expr)?) } else { None };
                         if name.is_none() && block.is_none() { Err(Loc(MISSING_NAME, word.1))? }
-                        Self::Expr(Expr::Module(word.1, block).named(name))
+                        Stmt::Expr(Expr::Module(word.1, block).named(name))
                     },
                     ItemWord::Object => {
                         let Some(expr) = expr else { Err(Loc(MISSING_EXPR, word.1))? };
@@ -171,7 +239,7 @@ impl Validate<Item> for Stmt {
                         let name = to_name(name)?;
                         let parameter = Pattern::from_expr(expr)?;
                         let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
-                        Self::Expr(Expr::object(word.1, parameter, block).named(name))
+                        Stmt::Expr(Expr::object(word.1, parameter, block).named(name))
                     },
                     ItemWord::Function | ItemWord::Macro => {
                         let Some(expr) = expr else { Err(Loc(MISSING_EXPR, word.1))? };
@@ -179,27 +247,27 @@ impl Validate<Item> for Stmt {
                         let (name, expr) = remove_call(expr)?;
                         let name = if let Some(name) = name { Some(to_name(name)?) } else { None };
                         let parameter = Pattern::from_expr(expr)?;
-                        Self::Expr(Expr::function(*word, parameter, return_type, block).named(name))
+                        Stmt::Expr(Expr::function(*word, parameter, return_type, block).named(name))
                     },
                     ItemWord::Trait => {
                         let Some(expr) = expr else { Err(Loc(MISSING_NAME, word.1))? };
                         let name = to_name(expr)?;
                         let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
-                        Self::Expr(Expr::Trait(word.1, block).named(name))
+                        Stmt::Expr(Expr::Trait(word.1, block).named(name))
                     },
                     ItemWord::Implementation => {
                         let Some(expr) = expr else { Err(Loc(MISSING_NAME, word.1))? };
                         let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
-                        Self::implementation(word.1, expr, block)
+                        Stmt::implementation(word.1, expr, block)
                     },
                     ItemWord::Return => {
                         if let Some(block) = block { Err(Loc(EXTRA_BLOCK, block.1))? }
-                        Self::return_(word.1, expr)
+                        Stmt::return_(word.1, expr)
                     },
                     ItemWord::Match => {
                         let Some(expr) = expr else { Err(Loc(MISSING_EXPR, word.1))? };
                         if let Some(block) = block { Err(Loc(EXTRA_BLOCK, block.1))? }
-                        Self::match_(word.1, expr)
+                        Stmt::match_(word.1, expr)
                     },
                     ItemWord::Case => {
                         let Some(expr) = expr else { Err(Loc(MISSING_EXPR, word.1))? };
@@ -208,25 +276,36 @@ impl Validate<Item> for Stmt {
                         let tag = to_tag(tag)?;
                         let parameter = Pattern::from_expr(expr)?;
                         let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
-                        Self::case(word.1, tag, parameter, block)
+                        Stmt::case(word.1, tag, parameter, block)
                     },
-                    ItemWord::If => todo!(),
-                    ItemWord::While => todo!(),
-                    ItemWord::For => todo!(),
-                    ItemWord::Else => todo!(),
+                    ItemWord::If => {
+                        let Some(expr) = expr else { Err(Loc(MISSING_NAME, word.1))? };
+                        let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
+                        Stmt::if_(word.1, expr, block)
+                    },
+                    ItemWord::While => {
+                        let Some(expr) = expr else { Err(Loc(MISSING_NAME, word.1))? };
+                        let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
+                        Stmt::while_(word.1, expr, block)
+                    },
+                    ItemWord::For => {
+                        let Some(expr) = expr else { Err(Loc(MISSING_NAME, word.1))? };
+                        let (pattern, sequence) = remove_in(expr)?;
+                        let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
+                        Stmt::for_(word.1, pattern, sequence, block)
+                    },
+                    ItemWord::Else => {
+                        if let Some(expr) = expr { Err(Loc(EXTRA_EXPR, expr.loc()))? }
+                        let Some(block) = block else { Err(Loc(MISSING_BLOCK, tree.loc()))? };
+                        return Ok(Self::Else(word.1, block));
+                    },
                 }
             },
             Item::Block(bracket) => Stmt::Block(Loc::<Block>::validate(bracket)?),
-            _ => { Err(Loc(MISSING_STMT, tree.loc()))? },
         };
-        Ok(ret)
+        Ok(Self::Stmt(ret))
     }
 }
-
-impl Validate<Doc<Item>> for Stmt {
-    fn validate(tree: &Doc<Item>) -> loc::Result<Self> { Ok(Self::validate(&tree.0)?) }
-}
-
 
 // ----------------------------------------------------------------------------
 
@@ -242,16 +321,25 @@ impl Validate<[Doc<Item>]> for Block {
     fn validate(tree: &[Doc<Item>]) -> loc::Result<Self> {
         let mut contents = Vec::new();
         let mut iter = tree.iter();
-        while let Some(item) = iter.next() {
-            let stmt = Stmt::validate(item)?;
-            if item.0.requires_semicolon() {
-                match iter.next() {
-                    Some(Doc(Item::Separator(Loc(Separator::Semicolon, _)), _)) => {},
-                    _ => { Err(Loc(MISSING_SEMICOLON, stmt.loc()))? },
+        while let Some(Doc(item, _)) = iter.next() {
+            match StmtOrElse::validate(item)? {
+                StmtOrElse::Stmt(stmt) => {
+                    if item.requires_semicolon() {
+                        match iter.next() {
+                            Some(Doc(Item::Separator(Loc(Separator::Semicolon, _)), _)) => {},
+                            _ => { Err(Loc(MISSING_SEMICOLON, stmt.loc()))? },
+                        }
+                    }
+                    contents.push(stmt);
+                },
+                StmtOrElse::Else(word, block) => {
+                    match contents.last_mut() {
+                        Some(Stmt::Control(_, _, _, Some(_))) => Err(Loc(EXTRA_ELSE, word))?,
+                        Some(Stmt::Control(_, _, _, e)) => { *e = Some(block); },
+                        _ => Err(Loc(EXTRA_ELSE, word))?,
+                    }
                 }
             }
-            // TODO: `else`.
-            contents.push(stmt);
         }
         Ok(Block(contents.into()))
     }
