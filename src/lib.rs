d400e5bd5894d5b02b3407de79bd05feefce8be3
Alistair Turnbull <github@minworks.co.uk>
2026-01-03 01:28:41 +0000

    Factor out `parse()`. Reimplement the REPL based on `welly_main::Repl`.

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,36 @@ pub mod enums;
 pub mod stream;
 pub mod lexer;
 pub mod parser;
-
 pub mod ast;
+
+pub const MISSING_STATEMENT: &'static str = "Expected a statement";
 
 // ----------------------------------------------------------------------------
 
-#[cfg(test)]
-mod tests {}
+use stream::{Stream, IteratorStream, CharIterator};
+use lexer::{Lexer};
+use parser::{Doc};
+use ast::{Validate, Stmt, Block};
+
+/// Attempt to lex, parse and validate `command`.
+pub fn parse(lexer: &Lexer, command: Loc<&str>) -> wm::Result<Box<[Stmt]>> {
+    // Lex.
+    let mut char_stream = IteratorStream::from(CharIterator::new(command));
+    let mut lexemes = Vec::new();
+    while !char_stream.is_empty() {
+        if let Some(l) = lexer.lex(&mut char_stream)? { lexemes.push(l); }
+    }
+    // Parse.
+    let mut lexeme_stream = IteratorStream::from(lexemes.into_iter());
+    let mut items = Vec::new();
+    while !lexeme_stream.is_empty() {
+        let Some(item) = Doc::parse(&mut lexeme_stream)? else {
+            // E.g. a `Lexeme::Close`.
+            let l = lexeme_stream.read()?;
+            Err(Loc(MISSING_STATEMENT, l.1))?
+        };
+        items.push(item);
+    }
+    // Validate.
+    Ok(Block::validate(&*items)?.0)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,21 @@
 use std::{io};
-use io::{BufRead, Write};
-use ansi_term::Colour::{Red, Blue};
+use io::{Write};
 
-use welly_main::{self as wm, ansi_term, Location, Loc};
-use welly_parser::{stream, lexer, parser, ast};
-use stream::{Stream, IteratorStream, CharIterator};
-use lexer::{Lexer};
-use parser::{Doc, Item};
-use ast::{Validate, Block};
-
-pub const MISSING_STATEMENT: &'static str = "Expected a statement";
+use welly_main::{ansi_term::Colour::{Blue}, Repl};
+use welly_parser::{lexer, parse};
 
 fn main() -> std::io::Result<()> {
+    let lexer = lexer::Lexer::default();
     let mut stdin = io::stdin().lock();
     let mut stdout = io::stdout();
-    echo(&mut stdin, &mut stdout)?;
-    Ok(())
-}
-
-/// Report an error, with an optional [`Location`].
-pub fn report(output: &mut impl Write, source: Loc<&str>, msg: &str, loc: Option<Location>) {
-    assert_eq!(source.0.len(), source.1.end - source.1.start);
-    // Ignore IO errors.
-    let _ = writeln!(output, "\n{}: {}",
-        Red.paint("Error"),
-        msg,
-    );
-    if let Some(loc) = loc {
-        let loc = loc - source.1.start;
-        let _ = writeln!(output, "{}{}{}",
-            &source.0[..loc.start],
-            Red.paint(&source.0[loc.start..loc.end]),
-            &source.0[loc.end..],
-        );
-    }
-}
-
-/// Lex, parse, validate and execute `command`.
-/// - history_len - the total length in bytes of the command history.
-pub fn run(output: &mut impl Write, lexer: &Lexer, command: Loc<&str>)
--> wm::Result<()> {
-    // Lex.
-    let mut lexemes = Vec::new();
-    let mut char_stream = IteratorStream::from(CharIterator::new(command));
-    while !char_stream.is_empty() {
-        if let Some(l) = lexer.lex(&mut char_stream)? { lexemes.push(l); }
-    }
-    // Parse.
-    let mut items = Vec::new();
-    let mut lexeme_stream = IteratorStream::from(lexemes.into_iter());
-    while !lexeme_stream.is_empty() {
-        let Some(item) = Doc::parse(&mut lexeme_stream)? else {
-            // E.g. a `Lexeme::Close`.
-            let l = lexeme_stream.read()?;
-            Err(Loc(MISSING_STATEMENT, l.1))?
-        };
-        items.push(item);
-    }
-    let items: Box<[Doc<Item>]> = items.into();
-    // Validate.
-    let stmts = Block::validate(&items)?.0;
-    // Show.
-    for stmt in stmts {
-        // Ignore IO errors.
-        let stmt_output = format!("{:#?}", stmt);
-        let _ = writeln!(output, "{}", Blue.paint(stmt_output));
-    }
-    Ok(())
-}
-
-pub fn echo<R: BufRead, W: Write>(input: &mut R, output: &mut W) -> io::Result<()> {
-    let mut history = String::new();
-    let mut command = String::new();
-    let mut is_complete = false;
-    let lexer = Lexer::default();
-    while !is_complete {
-        if command.is_empty() {
-            writeln!(output, "\nWelly!")?;
-        }
-        output.flush()?;
-        if input.read_line(&mut command)? == 0 { is_complete = true; }
-        let loc_command = Loc(command.as_str(), Location {start: history.len(), end: history.len() + command.len()});
-        match run(output, &lexer, loc_command) {
-            Ok(()) => {
-                history.push_str(&command);
-                command.clear();
-            },
-            Err(e) => {
-                if is_complete || !matches!(e, wm::Error::InsufficientInput) {
-                    e.report(|msg, loc| report(output, loc_command, msg, loc));
-                    command.clear();
-                }
-            },
+    let mut repl = Repl::default();
+    while !repl.is_complete {
+        writeln!(stdout, "\nWelly!")?;
+        if let Some(stmts) = repl.command(&mut stdin, &mut stdout, |command| parse(&lexer, command))? {
+            for stmt in stmts {
+                let stmt_output = format!("{:#?}", stmt);
+                writeln!(stdout, "{}", Blue.paint(stmt_output))?;
+            }
         }
     }
     Ok(())
